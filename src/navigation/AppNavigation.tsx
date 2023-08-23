e268b86c2437f6504d5c1a9bb15e4e4c72529180
vap1 <vaibhavvasant1705@gmail.com>
2023-08-24 00:38:18 +0530

    First Commit

---
+++ src/navigation/AppNavigation.tsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from '../screens/HomeScreen';
+import RegistrationScreen from '../screens/RegistrationScreen';
+import EmployeeManagementScreen from '../screens/EmployeeManagementScreen';
+import LeadManagementScreen from '../screens/LeadManagementScreen';
+import OpportunityTrackingScreen from '../screens/OpportunityTrackingScreen';
+import TaskAssignmentScreen from '../screens/TaskAssignmentScreen';
+
+const Stack = createStackNavigator();
+
+const AppNavigation: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="EmployeeManagement" component={EmployeeManagementScreen} />
+        <Stack.Screen name="LeadManagement" component={LeadManagementScreen} />
+        <Stack.Screen name="OpportunityTracking" component={OpportunityTrackingScreen} />
+        <Stack.Screen name="TaskAssignment" component={TaskAssignmentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
